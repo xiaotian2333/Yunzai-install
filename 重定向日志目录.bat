d301461c9eb15891f58bd685db8ac6bfb4f9676e
xiaotian <1719549416@qq.com>
2023-10-21 16:59:43 +0800

    新增日志目录重定向工具

---
+++ 重定向日志目录.bat
@@ -0,0 +1,27 @@
+:: 小天开发
+@echo off
+set tmp-path="A:"
+set name="新建文件夹"
+set /p name=请输入用户文件夹名:
+:: 云崽类型
+:: 1:Yunzai-Bot 2:Miao-Yunzai
+:: 默认使用喵崽
+set zai-type="2"
+echo ===请选择云崽类型===
+echo (1) Yunzai-Bot (2) Miao-Yunzai
+echo =====请输入选择=====
+set /p zai-type=等待输入:
+
+:: 处理云崽类型
+if "%zai-type%"=="1" (
+    set zai-name="Yunzai-Bot"
+)
+
+if "%zai-type%"=="2" (
+    set zai-name="Miao-Yunzai"
+)
+
+:: 日志重定向到内存盘
+mklink /d "%~dp0\%name%\%zai-name%\logs" "%tmp-path%\%name%"
+
+pause
